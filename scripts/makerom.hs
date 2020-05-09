44365713cdf0336139d318396192ce5488ee8316
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2020-05-09 16:17:53 +0300

    Add makerom.hs script, update .gitignore

---
+++ scripts/makerom.hs
@@ -0,0 +1,13 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+import Foreign.Marshal.Array (newArray)
+import Control.Exception (bracket)
+import Data.Int (Int8)
+import System.IO (IOMode(..), openBinaryFile, hClose, hPutBuf, hFlush)
+
+main :: IO ()
+main = bracket (openBinaryFile "rom.bin" WriteMode) hClose $ \handle -> do
+  let bytes = replicate 32768 (0xea :: Int8)
+  p <- newArray bytes
+  hPutBuf handle p (length bytes)
+  hFlush handle
